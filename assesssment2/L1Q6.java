655aad3833e3810201c1b3dca9929149eaf1b8c9
jessikasinghh <128406656+jessikasinghh@users.noreply.github.com>
2025-01-22 13:35:13 +0530

    DISCOUNT

---
+++ assesssment2/L1Q6.java
@@ -0,0 +1,10 @@
+public class L1Q6 {
+    public static void main(String[] args) {
+        double fee = 125000;
+        double discountPercent = 10;
+        double discount = (fee * discountPercent) / 100;
+        double finalFee = fee - discount;
+        System.out.println("The discount amount is " + discount + " and final discounted fee is " + finalFee);
+    }
+}
+
