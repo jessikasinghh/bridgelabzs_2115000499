7f9f24544da642666a415227a0057c7779790230
jessikasinghh <128406656+jessikasinghh@users.noreply.github.com>
2025-02-06 22:54:55 +0530

    Library and books(Aggregration)

---
+++ assessment9/Library_and_Books.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+
+class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
+
+class Library {
+    private ArrayList<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void removeBook(Book book) {
+        books.remove(book);
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "books=" + books +
+                '}';
+    }
+}
+
+public class Library_and_Books {
+    public static void main(String[] args) {
+        Book book1 = new Book("1984", "George Orwell");
+        Book book2 = new Book("To Kill a Mockingbird", "Harper Lee");
+        Book book3 = new Book("The Great Gatsby", "F. Scott Fitzgerald");
+
+        Library library1 = new Library();
+        Library library2 = new Library();
+
+        library1.addBook(book1);
+        library1.addBook(book2);
+        library2.addBook(book3);
+        System.out.println("Library 1: " + library1);
+        System.out.println("Library 2: " + library2);
+    }
+}
