f94f2cfdadc0392613631a4cd5b59c05b8fd7e72
jessikasinghh <128406656+jessikasinghh@users.noreply.github.com>
2025-02-18 22:36:33 +0530

    Smart Warehouse Management System

---
+++ assessment17/SmartWarehouseManagementSystem.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class WarehouseItem {
+    private String name;
+    private double price;
+
+    public WarehouseItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " {" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
+
+class Electronics extends WarehouseItem {
+    public Electronics(String name, double price) {
+        super(name, price);
+    }
+}
+
+class Groceries extends WarehouseItem {
+    public Groceries(String name, double price) {
+        super(name, price);
+    }
+}
+
+class Furniture extends WarehouseItem {
+    public Furniture(String name, double price) {
+        super(name, price);
+    }
+}
+
+class Storage<T extends WarehouseItem> {
+    private List<T> items = new ArrayList<>();
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public void removeItem(T item) {
+        items.remove(item);
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+}
+
+class WarehouseUtils {
+    public static void displayItems(List<? extends WarehouseItem> items) {
+        for (WarehouseItem item : items) {
+            System.out.println(item);
+        }
+    }
+}
+
+public class SmartWarehouse {
+    public static void main(String[] args) {
+        Storage<Electronics> electronicsStorage = new Storage<>();
+        Storage<Groceries> groceriesStorage = new Storage<>();
+        Storage<Furniture> furnitureStorage = new Storage<>();
+
+        electronicsStorage.addItem(new Electronics("Laptop", 800.00));
+        electronicsStorage.addItem(new Electronics("Smartphone", 500.00));
+
+        groceriesStorage.addItem(new Groceries("Apple", 2.00));
+        groceriesStorage.addItem(new Groceries("Milk", 1.50));
+
+        furnitureStorage.addItem(new Furniture("Chair", 120.00));
+        furnitureStorage.addItem(new Furniture("Table", 250.00));
+
+        System.out.println("Electronics:");
+        WarehouseUtils.displayItems(electronicsStorage.getItems());
+
+        System.out.println("\nGroceries:");
+        WarehouseUtils.displayItems(groceriesStorage.getItems());
+
+        System.out.println("\nFurniture:");
+        WarehouseUtils.displayItems(furnitureStorage.getItems());
+    }
+}
