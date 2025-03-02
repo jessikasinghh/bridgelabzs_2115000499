8fb20b62cba9f8763c88cb547abd0be24ef39530
jessikasinghh <128406656+jessikasinghh@users.noreply.github.com>
2025-03-02 22:22:01 +0530

    Validate a Hex Color Code

---
+++ Regex/HexColor.java
@@ -0,0 +1,12 @@
+
+public class HexColorValidator {
+    public static boolean isValidHexColor(String color) {
+        return color.matches("^#([A-Fa-f0-9]{6})$");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValidHexColor("#FFA500")); // true
+        System.out.println(isValidHexColor("#ff4500")); // true
+        System.out.println(isValidHexColor("#123"));    // false
+    }
+}
